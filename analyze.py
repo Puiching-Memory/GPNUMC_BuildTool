ba9241d106cc2632d21fc9103cdd854302f13044
梦归云帆 <1138663075@qq.com>
2023-10-11 22:14:32 +0800

    Fix Bug

--- analyze.py
+++ analyze.py
@@ -10,10 +10,18 @@ Python Version:3.11.5
 
 import output
 import objloadN
-import numpy
 
-Object = objloadN.OBJN("./3dobj/3F.obj")
+##import numpy
+
+infile_name = input("输入读取文件路径:")
+outfile_name = input("输入导出文件名：***.schem:\n")
+if outfile_name == "":
+    outfile_name == "out"
+
+print("正在分析OBJ...")
+Object = objloadN.OBJN(infile_name)
 data = Object.Exp_V()
 
+print("正在导出文件：", outfile_name + ".schem")
 out = output.SchemN(data)
-out.Exp_sch()
+out.Exp_sch(outfile_name)
--- output.py
+++ output.py
@@ -13,12 +13,12 @@ class SchemN():
     def __init__(self,data:list) -> None:
         self.data = data
 
-    def Exp_sch(self):
+    def Exp_sch(self,filename:str):
         schem = mcschematic.MCSchematic()
         for i in self.data:
-            schem.setBlock(  (int(i[0]), int(i[2]), int(i[1])), "minecraft:stone"  )
+            schem.setBlock(  (int(i[1]), int(i[2]), int(i[0])), "minecraft:stone"  )
 
-        schem.save(  "./", "test", mcschematic.Version.JE_1_20_1)
+        schem.save(  "./", filename, mcschematic.Version.JE_1_20_1)
 
 
 
